bb1ae632efbf45348266294ad68fa157a14d9d5e
MakyJame <ngoctai1995@gmail.com>
2026-06-25 21:24:02 +0700

    Change 101 for actions github

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.include_router(user_router)
 
 @app.get("/")
 def home():
-    return {"msg":"hello from home 100"}
+    return {"msg":"hello from home 101"}
 
 @app.get("/login")
 def login():
