3454a569f2bed6e461beb2f22ea72a04e8dad67f
MakyJame <ngoctai1995@gmail.com>
2026-06-25 21:40:45 +0700

    Test github actions 104

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.include_router(user_router)
 
 @app.get("/")
 def home():
-    return {"msg":"hello from home 103"}
+    return {"msg":"hello from home 104"}
 
 @app.get("/login")
 def login():
